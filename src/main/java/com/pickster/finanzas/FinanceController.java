285e843d937a2f7318d0bf8e3ef5fa980d8e4b4d
eduardo.juarez <edjt.1216@gmail.com>
2026-06-24 19:46:39 -0600

    Support legacy finance API prefix

--- src/main/java/com/pickster/finanzas/FinanceController.java
+++ src/main/java/com/pickster/finanzas/FinanceController.java
@@ -9,7 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/finance")
+@RequestMapping({"/api/finance", "/finance"})
 public class FinanceController {
     private final FinanceService service;
 
